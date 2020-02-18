a3c7639e9647c206c9dd92862d6e7a009c001d5d
gnieuwerth <g.nieuwerth@hotmail.com>
2020-02-18 12:44:52 +0100

    Generated NACA0008 and NACA4408 data files. Updated assignment script to use alpha in degrees Updated assignment script to use NACA4408 instead of Eppler

--- thin_airfoil_dvm/assignment_1_script.py
+++ thin_airfoil_dvm/assignment_1_script.py
@@ -8,17 +8,16 @@ from thin_airfoil_dvm_main import thin_airfoil_dvm
 # =================================================================================================
 
 # -------------------------------------------------------------------------------------------------
-# Eppler 553 pressure distribution
+# NACA4408 pressure distribution
 # -------------------------------------------------------------------------------------------------
 
-airfoilname = "e553"
-alpha_deg = 5.
-alpha = alpha_deg * np.pi / 180.
+airfoilname = "NACA4408"
+alpha = 5.
 q_inf = 5.
 n_panels = 50
 rho = 1.225
 
-eppler_cp = thin_airfoil_dvm(airfoilname, alpha, q_inf, n_panels, rho)[0]
+NACA_cp = thin_airfoil_dvm(airfoilname, alpha, q_inf, n_panels, rho)[0]
 
 # -------------------------------------------------------------------------------------------------
 # Flat plate pressure distribution
@@ -33,8 +32,8 @@ airfoilname = "flat_plate"
 flat_plate_cp = thin_airfoil_dvm(airfoilname, alpha, q_inf, n_panels, rho)[0]
 
 plt.figure()
-plt.title(r" Eppler 553 vs Flat plate pressure distribution, $\alpha =$ " + str(alpha_deg) + r"$ ^{\circ} $")
-plt.plot(np.linspace(0, 1, n_panels), eppler_cp, 'o',  label='Eppler 553')
+plt.title(r" NACA 4408 vs Flat plate pressure distribution, $\alpha =$ " + str(alpha) + r"$ ^{\circ} $")
+plt.plot(np.linspace(0, 1, n_panels), NACA_cp, 'o',  label='NACA 4408')
 plt.plot(np.linspace(0, 1, n_panels), flat_plate_cp, 'x', label='Flat plate')
 plt.xlabel(r'$ \frac{x}{c}$')
 plt.ylabel(r'$\Delta C_p$')
@@ -45,21 +44,20 @@ plt.legend()
 # -------------------------------------------------------------------------------------------------
 
 
-alphas_deg = np.linspace(-5., 15., 100)
-alphas = alphas_deg * np.pi / 180.
+alphas = np.linspace(-5., 15., 100)
 N_panels = 50
 
 # -------------------------------------------------------------------------------------------------
-# Eppler lift polar
+# NACA4408 lift polar
 # -------------------------------------------------------------------------------------------------
 
-airfoilname = "e553"
-Cls_eppler = []
+airfoilname = "NACA4408"
+Cls_NACA = []
 
 for i in alphas:
 
     Cl = thin_airfoil_dvm(airfoilname, i, q_inf, n_panels, rho)[1]
-    Cls_eppler.append(Cl)
+    Cls_NACA.append(Cl)
 
 # -------------------------------------------------------------------------------------------------
 # Flat plate lift polar
@@ -78,9 +76,9 @@ for i in alphas:
 # -------------------------------------------------------------------------------------------------
 
 plt.figure()
-plt.title("Lift polar comparison: Eppler 553 vs Flat plate")
-plt.plot(alphas_deg, Cls_eppler, label='Eppler 553')
-plt.plot(alphas_deg, Cls_flat, label='Flat plate')
+plt.title("Lift polar comparison: NACA 4408 vs Flat plate")
+plt.plot(alphas, Cls_NACA, label='NACA 4408')
+plt.plot(alphas, Cls_flat, label='Flat plate')
 plt.xlabel(r"Angle of attack $[^{\circ}]$")
 plt.ylabel(r"$C_l [-]$")
 plt.legend()
@@ -93,10 +91,9 @@ plt.legend()
 # -------------------------------------------------------------------------------------------------
 
 q_inf = 5.
-alpha_deg = 5.
-alpha = alpha_deg * np.pi / 180.
+alpha = 5.
 rho = 1.225
-airfoilname = "e553"
+airfoilname = "NACA4408"
 
 # -------------------------------------------------------------------------------------------------
 # Low and high density data sets
@@ -111,7 +108,7 @@ cp_high = thin_airfoil_dvm(airfoilname, alpha, q_inf, n_high, rho)[0]
 # Lift polar plots
 # -------------------------------------------------------------------------------------------------
 plt.figure()
-plt.title(r" Effect of panel density on Eppler 553 pressure distribution, $\alpha =$ " + str(alpha_deg) + r"$ ^{\circ} $")
+plt.title(r" Effect of panel density on NACA 4408 pressure distribution, $\alpha =$ " + str(alpha) + r"$ ^{\circ} $")
 plt.plot(np.linspace(0, 1, n_low), cp_low, 'o',  label='N_panels = {}'.format(n_low))
 plt.plot(np.linspace(0, 1, n_high), cp_high, 'x', label='N_panels ={}'.format(n_high))
 plt.xlabel(r'$ \frac{x}{c}$')
